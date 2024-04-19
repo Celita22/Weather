207a18c6ced76ef6b6501e6b6f9037d936eefc8a
Celita22 <141368481+Celita22@users.noreply.github.com>
2024-04-20 00:35:54 +0100

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,7 @@
+
+function mudar (){
+document.getElementById('modo-claro-escuro').addEventListener('click', function() {
+    var icone = document.getElementById('icone-modo');
+    icone.classList.toggle('icone-ativo');
+});
+}
