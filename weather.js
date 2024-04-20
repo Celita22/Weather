19816ec02c99c58b24e59d7953c0f9a0a772e61b
Celita Domingos <domingoscelita@gmail.com>
2024-04-20 15:56:32 +0100

    actualizado

--- weather.js
+++ weather.js
@@ -1,11 +1,33 @@
-var API_CHAVE = "495de84d4964370a1a9c1a9991578169";
-var cidade = document.querySelector(".city-input");
-var pesquisar = document.querySelector(".search-btn");
-var localizaoAtual = document.querySelector(".location-btn");
-var tempoAtual = document.querySelector(".clima-atual");
-var climaCards = document.querySelector(".clima-cards");
+const API_CHAVE = "495de84d4964370a1a9c1a9991578169";
+const cidade = document.querySelector(".city-input");
+const pesquisar = document.querySelector(".search-btn");
+const localizaoAtual = document.querySelector(".location-btn");
+const tempoAtual = document.querySelector(".clima-atual");
+const climaCards = document.querySelector(".clima-cards");
 
-const diasDaSemana = ["Domingo","Segunda-Feira","Terça-Feira","Quarta-Feira","Quinta-Feira","Sexta-Feira","Sábado"];
+const diasDaSemana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
+
+function pegarCoordenadasUsuario() {
+    navigator.geolocation.getCurrentPosition(localizacao => {
+        var { latitude, longitude } = localizacao.coords;
+        const API_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_CHAVE}`;
+        fetch(API_URL).then(response => response.json()).then(dados => {
+            if (dados.length == 0) {
+                alert('Nenhuma informação encontrada para essa cidade');
+                return;
+            }
+            var { name } = dados[0];
+            console.log(dados);
+            buscarDetalhesTempo(name, latitude, longitude);
+
+
+        }).catch(() => {
+            alert("Algum erro");
+        });
+
+    })
+
+}
 
 function buscarDetalhesTempo(nomeCidade, latitude, longitude) {
 
@@ -13,9 +35,13 @@ function buscarDetalhesTempo(nomeCidade, latitude, longitude) {
 
     fetch(CLIMA_API_URL).then(response => response.json()).then(dados => {
         const atualProximosDias = [];
+        var days = [];
         const fiveDaysForecast = dados.list.filter(forecast => {
             const forecastDate = new Date(forecast.dt_txt).getDate();
+            var dia = new Date(forecast.dt_txt).getDay();
+
             if (!atualProximosDias.includes(forecastDate)) {
+                days.push(diasDaSemana[dia]);
                 return atualProximosDias.push(forecastDate);
             }
         });
@@ -24,26 +50,27 @@ function buscarDetalhesTempo(nomeCidade, latitude, longitude) {
         climaCards.innerHTML = "";
 
         fiveDaysForecast.forEach((item, index) => {
-            const html = criarCardClima(nomeCidade, item, index);
+            const html = criarCardClima(nomeCidade, item, index, days);
             if (index === 0) {
                 tempoAtual.insertAdjacentHTML("beforeend", html);
             } else {
                 climaCards.insertAdjacentHTML("beforeend", html);
             }
-        });        
+        });
     }).catch(() => {
         alert("Algum erro");
     });
 
 }
 
-function criarCardClima(cidade, item, index) {
+function criarCardClima(cidade, item, index, dias) {
     if (index == 0) {
         return `<div class="details">
                     <h2>${cidade}</h2>
                     <h6>Temperatura: ${(item.main.temp - 273.15).toFixed(2)}°C</h6>
                     <h6>Ventania:${(item.wind.speed)} m/s</h6>
                     <h6>Humidade:${(item.main.humidity)}%</h6>
+                    <h6>Hora:${(item.dt_txt.split(" ")[1])}</h6>
                 </div> 
                 <div class="icon">
                     <img src="https://openweathermap.org/img/wn/${(item.weather[0].icon)}@4x.png">
@@ -52,7 +79,7 @@ function criarCardClima(cidade, item, index) {
 
     } else {
         return `<div class="card">
-                <h2>${cidade}</h2>
+                <h2>${(dias[index])}</h2>
                 <img src="https://openweathermap.org/img/wn/${(item.weather[0].icon)}@4x.png">
                 <h6>${item.weather[0].description}</h6>
                 <h6>Temperatura: ${(item.main.temp - 273.15).toFixed(2)}°C</h6>
@@ -76,7 +103,7 @@ function dadosCidade() {
         }
         var { lat, lon, name } = dados[0];
         buscarDetalhesTempo(name, lat, lon);
-        
+
     }).catch(() => {
         alert("Algum erro");
     });
@@ -84,4 +111,5 @@ function dadosCidade() {
 }
 
 pesquisar.addEventListener("click", dadosCidade);
-
+localizaoAtual.addEventListener("click", pegarCoordenadasUsuario)
+pegarCoordenadasUsuario();
